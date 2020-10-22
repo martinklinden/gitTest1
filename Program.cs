900715eca95b93947b346155a41a2e3fcc676c4c
Martin Lindén <MaritnI@hotmail.com>
2020-10-22 10:10:36 +0200

    Nostalgiloopen tillagd

--- Program.cs
+++ Program.cs
@@ -6,8 +6,11 @@ namespace gitTest1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
-            Console.WriteLine("Extra text");
+            //Console.WriteLine("Hello world!");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0}^2 = {1}", i, i * i);
+            }
         }
     }
 }
