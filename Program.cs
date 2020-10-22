24c19715f9f2d5e034d28ddac8db19e33518cf7c
Martin Lindén <MaritnI@hotmail.com>
2020-10-22 10:47:08 +0200

    Enhanced loop printout with cube

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace gitTest1
             //Console.WriteLine("Hello world!");
             for (int i = 1; i <= 10; i++)
             {
-                Console.WriteLine("{0}^2 = {1}", i, i * i);
+                Console.WriteLine("{0}^2 = {1}, {0}^3 = {2}", i, i*i, i*i*i);
                 Console.ReadKey();
             }
         }
