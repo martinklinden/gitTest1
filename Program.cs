b208fe161e18de8f33714a9101902b9600e56b54
Martin Lindén <MaritnI@hotmail.com>
2020-10-22 10:59:03 +0200

    Avnostalgisering startad

--- Program.cs
+++ Program.cs
@@ -6,10 +6,9 @@ namespace gitTest1
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello world!");
             for (int i = 1; i <= 10; i++)
             {
-                Console.WriteLine("{0}^2 = {1}, {0}^3 = {2}", i, i*i, i*i*i);
+                Console.WriteLine("sqrt{0} = {1}", i,Math.Sqrt(i));
                 Console.ReadKey();
             }
         }
